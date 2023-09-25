c13cf9b326fc04620d2b12dd3d0bdd5c68a611be
imon saha <imonsaha153@gmail.com>
2023-09-25 21:19:16 +0600

    mx auto update

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const handleCatageroy = async ()=>{
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const data = await res.json()
     const category = data.data
-    console.log(category)
+    // console.log(category)
     //  console.log(data)
     const tabContainer = document.getElementById('tab-container')
     category.forEach(category => {
         const div = document.createElement('div')
         div.innerHTML=`
-        <a onclick="showDetails('${category.category_id}')" class="tab">${category.category}</a> 
+        <a onclick="showDetails('${category.category_id}')" class="tab ">${category.category}</a> 
         `
         tabContainer.appendChild(div)
         
@@ -28,15 +28,19 @@ const showDetails = async (categoryId) =>{
         div.innerHTML = `
         
         
-        <div class="card bg-base-100 shadow-xl container mx-auto grow">
-      <figure><img src="${news?.thumbnail}" /></figure>
+        <div class="card bg-base-100 shadow-xl ">
+        <div class="h-52">
+        <figure><img  src="${news?.thumbnail}" /></figure>
+  
+      </div>
       <div class="card-body">
         <h2 class="card-title">${news?.title}</h2>
         <div class="flex justify-center">
-      <div class="w-16 justify-center">
+       <div class="w-16 justify-center">
         <figure><img class="rounded-full"  src="${news?.authors[0].profile_picture}" /></figure>
-      </div>
-      <p class="items-center m-auto ml-2">${news?.authors[0].profile_name}</p>
+        </div>
+      <p class="items-center m-auto ml-2">${news?.authors[0].profile_name}</p> </br>
+      <h2>Views:${news?.others.views}</h2>
     </div>
         
         
@@ -55,6 +59,7 @@ const handledBlog = ()=>{
   const blog = document.getElementById('blog-container')
   blog = window.location.href='blog.html'
 }
+
 
 handleCatageroy()
 showDetails("1000")
